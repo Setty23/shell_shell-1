cb0225312bdb03caa469ceb03bba72d49b811634
Setty23 <125440661+Setty23@users.noreply.github.com>
2023-08-23 22:18:45 +0300

    Update vars.c

--- simcorrect1/vars.c
+++ simcorrect1/vars.c
@@ -8,7 +8,7 @@
  *
  * Return: 1 if chain delimeter, 0 if not
  */
-int is_chain(info_t *info, char *buf, int *p)
+int is_chain(info_t *info, char *buf, size_t *p)
 {
 	char *chain_delims = ";&|";
 	char c = buf[*p];
