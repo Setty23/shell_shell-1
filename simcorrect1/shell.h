e8378208d235cd6747936bb6349ac3ad54afc258
Setty23 <125440661+Setty23@users.noreply.github.com>
2023-08-23 11:51:37 +0300

    Update shell.h

--- simcorrect1/shell.h
+++ simcorrect1/shell.h
@@ -115,7 +115,7 @@ typedef struct builtin
 int hsh(info_t *, char **);
 int find_builtin(info_t *);
 void find_cmd(info_t *);
-void fork_cmd(info_t *);
+void exec_cmd(info_t *);
 
 /* shell_pointer.c */
 int is_cmd(info_t *, char *);
