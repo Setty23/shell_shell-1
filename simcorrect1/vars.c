ad95147fe1996debcab6ec87f200060c32cf866b
Setty23 <125440661+Setty23@users.noreply.github.com>
2023-08-23 23:12:57 +0300

    Update vars.c

--- simcorrect1/vars.c
+++ simcorrect1/vars.c
@@ -47,7 +47,6 @@ int is_chain(info_t *info, char *buf, size_t *p)
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
 	size_t x = *p;
-	int status = 0;
 
 	if (info->cmd_buf_type == CMD_AND)
 	{
